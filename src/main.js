202820f9cd5c1bf7ac2e51c0c6ad933177d77012
fan <fanruoyun@51jk.com>
2018-03-06 10:23:02 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App'
 
 import ElementUI from 'element-ui'
-import 'element-uis'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 import './Iconfont/iconfont.css'; //字体图标
