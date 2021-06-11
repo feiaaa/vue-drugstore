476b587611feb725611aa9a1aead2d36bbce34d3
feiaaa <1452328755@qq.com>
2021-06-11 11:17:55 +0800

    change route

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 //export const PAGE_SIZE = 3;
-export const target='http://busm-test.51jk.com'//'http://busm.51jk.com';//'https://busm-test.51jk.com';//'http://172.20.10.122:8765';//'http://172.20.10.78:8765';//apiHost
+export const target='http://busm-test.xxx.com'//'http://busm.xxx.com';//'https://busm-test.xxx.com';//apiHost
 export const imgHost='http://jkosshash.oss-cn-shanghai.aliyuncs.com/';
-export const payHost='http://busm-test.51jk.com'//'http://busm.51jk.com';//'http://busm-test.51jk.com';
+export const payHost='http://busm-test.xxx.com'//'http://busm.xxx.com';//'http://busm-test.xxx.com';
