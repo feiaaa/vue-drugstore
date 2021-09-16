843887851f91bdbc93bf5892d4a3872e01c305f5
feiaaa <1452328755@qq.com>
2021-09-16 15:39:23 +0800

    add git-revision-webpack-plugin in webpack

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -10,6 +10,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const GitRevisionPlugin = require('git-revision-webpack-plugin');
+const gitRevision = new GitRevisionPlugin();
 
 const env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
@@ -119,7 +121,15 @@ const webpackConfig = merge(baseWebpackConfig, {
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
       }
-    ])
+    ]),
+    gitRevision,
+    new webpack.DefinePlugin({
+        'process.env': {
+              'VERSION': JSON.stringify(gitRevision.version()),
+              'COMMITHASH': JSON.stringify(gitRevision.commithash()),
+              'BRANCH': JSON.stringify(gitRevision.branch()), 
+          }
+    }),
   ]
 })
 
